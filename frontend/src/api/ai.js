653f5a59d5ee3caf3ac0b06d40fbedf6b41e67ce
VickyGirase <vickygirase3908@gmail.com>
2025-09-13 13:24:32 +0530

    minor

--- frontend/src/api/ai.js
+++ frontend/src/api/ai.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://ai-code-comment-generator-rrr3.vercel.app/";
+const API_URL = "https://ai-code-comment-generator-rrr3.vercel.app";
 
 export const generateComments = async ({ codeSnippet, language }) => {
   const res = await axios.post(`${API_URL}/generate-comments`, { codeSnippet, language });
