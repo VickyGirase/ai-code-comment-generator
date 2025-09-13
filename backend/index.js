04cd0163357c178f29f3cff568379049f9b2b2de
VickyGirase <vickygirase3908@gmail.com>
2025-09-13 11:29:11 +0530

    minor

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 import serverless from "serverless-http";
 import app from "./server.js";
 
-// export serverless handler
+// For Vercel
 export const handler = serverless(app);
+
+// For local development
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`✅ Server running locally on http://localhost:${PORT}`);
+  });
+}
